b610de42ae40851d0432f28f85b50719808820d1
Leonardo Scholler <leonardo.scholler@poatek.com>
2021-10-31 17:40:03 -0300

    refined logic and added scale

--- Car.py
+++ Car.py
@@ -11,68 +11,73 @@ class AIType(Enum):
   PLAYER = 1
   ENEMY = 2
 
-CAR_BODY = [Vector(-2, -2), Vector(2, -2), Vector(0, 4)]
+CAR_BODY = [Vector(-1, -1), Vector(1, -1), Vector(0, 2)]
 SPEED = 0.1
+SIZE = 1.5
 
 class Car:
   def __init__(self, type = AIType.ENEMY):
     self.type = type
-    self.Road = None
+    self.road = None
     self.next = None
-    self.Vertices = copy.deepcopy(CAR_BODY)
+    self.vertices = copy.deepcopy(CAR_BODY)
     self.position = 0
     self.direction = -1
     self.moving = True
 
   def setStart(self, road=None, position=0):
-    self.Road = road
+    self.road = road
     self.position = position
   
   def render(self):
     if(self.type == AIType.PLAYER): glColor3d(0, 1, 0)
     else: glColor3d(1, 0, 0)
     glPushMatrix()
-    point = self.Road.getPoint(self.position)
+    point = self.road.getPoint(self.position)
     glTranslate(point.x, point.y, 0)
 
-    direction = self.Road.tangent(self.position)
+    glScale(SIZE, SIZE, 1)
+
+    direction = self.road.tangent(self.position)
     rotation = math.degrees(math.atan2(direction.x, direction.y))
     glRotate(-rotation + (0 if (self.direction == 1) else 180), 0, 0, 1)
 
     glBegin(GL_POLYGON)
-    [glVertex3f(p.x, p.y, 0) for p in self.Vertices]
+    [glVertex3f(p.x, p.y, 0) for p in self.vertices]
     glEnd()
     glPopMatrix()
 
   def move(self):
     movement = SPEED * self.direction
-    self.position += movement / self.Road.length
+    self.position += movement / self.road.length
     if(self.direction == 1):
       if(self.position > 0.5 and self.next == None): 
         self.chooseNext()
       elif(self.position > 1):
-        self.Road = self.next
+        self.road = self.next
         self.position = 0
         self.next = None
+        if(self.type == AIType.PLAYER): self.road.selected = False
     else:
       if(self.position < 0.5 and self.next == None): 
         self.chooseNext()
       elif(self.position < 0):
-        self.Road = self.next
+        self.road = self.next
         self.position = 1
         self.next = None
-        self.Road.selected = False
+        if(self.type == AIType.PLAYER): self.road.selected = False
 
   def chooseNext(self):
     if(self.direction == 1):
-      size = len(self.Road.connectedForeward)
+      size = len(self.road.connectedForeward)
       next = random.randint(0, size-1)
-      self.next = list(self.Road.connectedForeward)[next]
+      self.next = list(self.road.connectedForeward)[next]
     else:
-      size = len(self.Road.connectedBackward)
+      size = len(self.road.connectedBackward)
       next = random.randint(0, size-1)
-      self.next = list(self.Road.connectedBackward)[next]
-    self.next.selected = True
+      self.next = list(self.road.connectedBackward)[next]
+    
+    if(self.type == AIType.PLAYER): self.next.selected = True
 
   def __str__(self):
-    return f"Road: {self.Road}\nNext: {self.next}\nPosition: {self.position}\nMoving: {self.moving}\nDirection: {self.direction}\nType: {self.type}"
+    return f"Road: {self.road}\nNext: {self.next}\nPosition: {self.position}\nMoving: {self.moving}\nDirection: {self.direction}\nType: {self.type}"
