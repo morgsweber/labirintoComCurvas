036b6b2acc0f1ee22d14b8eee92fe4d583c7043f
Leonardo Scholler <leonardo.scholler@poatek.com>
2021-11-01 10:25:08 -0300

    fixed connections

--- Connection.py
+++ Connection.py
@@ -4,9 +4,9 @@ class Connection:
     self.bias = -1 if inverted else 1
     
   def addToForeward(self, road):
-    if(all([road != connection.road for connection in road.connectedForeward])):
+    if(all([self.road != connection.road for connection in road.connectedForeward])):
       road.connectedForeward += [self]
 
   def addToBackward(self, road):
-    if(all([road != connection.road for connection in road.connectedForeward])):
+    if(all([self.road != connection.road for connection in road.connectedBackward])):
       road.connectedBackward += [self]
